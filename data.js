2f22a114816fe0979ca5e8cda6063d290156af7f
Emmanuel <yournewemail@example.com>
2025-04-17 00:11:53 -0400

    preload

---
+++ data.js
@@ -0,0 +1,45 @@
+async function track() {
+    try {
+      document.getElementById('submit_wpcargo').value = 'Wait'
+      const trackInput = document.getElementById('trackingcode').value;
+  
+      if (trackInput === '') {
+        return alert ('Tracking Number Requred!!')
+      }
+  
+      console.log(trackInput);
+  
+      const url = 'https://slada.onrender.com/admin/id-info/';
+      const data = {
+        trackNumber: trackInput,
+      };
+  
+      // console.log(JSON.stringify(data));
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          "Access-Control-Allow-Origin-": "no-cors",
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      });
+  
+      const doc = await response.json();
+  
+      console.log(doc);
+  
+      if (response.status !== 200) {
+        document.getElementById('submit_wpcargo').value = 'Refresh'
+        return alert(doc.message);
+      }
+  
+    //   document.getElementById('submit_wpcargo').value = 'TRACK RESULT'
+      sessionStorage.setItem('doc', JSON.stringify(doc));
+      document.location.href = '/track-data.html';
+    } catch (error) {
+      document.getElementById('submit_wpcargo').value = 'Refresh'
+      console.log(error);
+    }
+  };
+  
